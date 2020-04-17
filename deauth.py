2f9a8ba6970ef99ca80eab62ee0cf65b7ca2d0f7
dadav <33197631+dadav@users.noreply.github.com>
2020-04-17 22:38:37 +0200

    add deauth counter

---
+++ deauth.py
@@ -0,0 +1,45 @@
+import logging
+
+from pwnagotchi import plugins
+from pwnagotchi.ui import fonts
+from pwnagotchi.ui.components import LabeledValue
+from pwnagotchi.ui.view import BLACK
+
+
+class Deauth(plugins.Plugin):
+    __author__ = 'scorp'
+    __version__ = '1.0.0'
+    __name__ = 'deauthcounter'
+    __license__ = 'MIT'
+    __description__ = 'counts the successful deauth attacks of this session '
+
+    def __init__(self):
+        self.deauth_counter = 0
+        self.handshake_counter = 0
+
+    def on_loaded(self):
+        logging.info("[scorp_deauthcounter] loaded")
+
+    # called to setup the ui elements
+    def on_ui_setup(self, ui):
+        # add custom UI elements
+        ui.add_element('deauth', LabeledValue(color=BLACK, label='Deauths ', value=str(self.deauth_counter),
+                                              position=(ui.width() / 2 + 50, ui.height() - 25),
+                                              label_font=fonts.Bold, text_font=fonts.Medium))
+        ui.add_element('hand', LabeledValue(color=BLACK, label='Handshakes ', value=str(self.handshake_counter),
+                                            position=(ui.width() / 2 + 50, ui.height() - 35),
+                                            label_font=fonts.Bold, text_font=fonts.Medium))
+
+    # called when the ui is updated
+    def on_ui_update(self, ui):
+        # update those elements
+        ui.set('deauth', str(self.deauth_counter))
+        ui.set('hand', str(self.handshake_counter))
+
+    # called when the agent is deauthenticating a client station from an AP
+    def on_deauthentication(self, agent, access_point, client_station):
+        self.deauth_counter += 1
+
+    def on_handshake(self, agent, filename, access_point, client_station):
+        logging.info("!!!!!!!!!!!!!")
+        self.handshake_counter += 1
