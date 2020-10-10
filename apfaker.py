1ca3fffad2c869b30ef3d282856008af9d121b17
dadav <33197631+dadav@users.noreply.github.com>
2020-10-10 15:14:21 +0200

    switch to scapy

--- apfaker.py
+++ apfaker.py
@@ -1,41 +1,66 @@
 import os
 import logging
 
-from random import randint, choice, shuffle
+from random import shuffle
 from pwnagotchi import plugins
 from pwnagotchi.ui.components import LabeledValue
 from pwnagotchi.ui.view import BLACK
 from pwnagotchi.ui import fonts
+from time import sleep
+from scapy.all import Dot11,Dot11Beacon,Dot11Elt,RadioTap,sendp, RandMAC
 
 
 class APFaker(plugins.Plugin):
     __author__ = '33197631+dadav@users.noreply.github.com'
-    __version__ = '1.1.5'
+    __version__ = '2.0.0'
     __license__ = 'GPL3'
     __description__ = 'Creates fake aps.'
+    __dependencies__ = {
+        'pip': ['scapy'],
+    }
     __defaults__ = {
         'enabled': False,
         'ssids': ['5G TEST CELL TOWER'],
-        'max': 10,
+        'max': 100,
         'repeat': True,
+        'password_protected': False,
     }
-
-    @staticmethod
-    def random_mac():
-        return "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}".format(randint(0, 255),
-                                                                  randint(0, 255),
-                                                                  randint(0, 255),
-                                                                  randint(0, 255),
-                                                                  randint(0, 255),
-                                                                  randint(0, 255))
 
     def __init__(self):
         self.options = dict()
         self.ready = False
+        self.shutdown = False
+
+    @staticmethod
+    def create_beacon(name, password_protected=False):
+        dot11 = Dot11(type=0,
+                      subtype=8,
+                      addr1='ff:ff:ff:ff:ff:ff',
+                      addr2=str(RandMAC()),
+                      addr3=str(RandMAC()))
+
+        beacon = Dot11Beacon(cap='ESS+privacy' if password_protected else 'ESS')
+        essid = Dot11Elt(ID='SSID',info=name, len=len(name))
+
+        if not password_protected:
+            return RadioTap()/dot11/beacon/essid
+
+        rsn = Dot11Elt(ID='RSNinfo', info=(
+                       '\x01\x00'
+                       '\x00\x0f\xac\x02'
+                       '\x02\x00'
+                       '\x00\x0f\xac\x04'
+                       '\x00\x0f\xac\x02'
+                       '\x01\x00'
+                       '\x00\x0f\xac\x02'
+                       '\x00\x00'))
+
+        return RadioTap()/dot11/beacon/essid/rsn
 
     def on_loaded(self):
         if isinstance(self.options['ssids'], str):
             path = self.options['ssids']
+
             if not os.path.exists(path):
                 self.ssids = [path]
             else:
@@ -54,6 +79,7 @@ class APFaker(plugins.Plugin):
         self.ready = True
         logging.info('[apfaker] plugin loaded')
 
+
     def on_ready(self, agent):
         if not self.ready:
             return
@@ -66,18 +92,23 @@ class APFaker(plugins.Plugin):
             self.ssids.extend([f"{ssid}_{cnt}" for ssid in base_list])
             cnt += 1
 
+        frames = list()
         for idx, ssid in enumerate(self.ssids[:self.options['max']]):
-            channel = choice([1, 6, 11])
-            mac = APFaker.random_mac()
             try:
-                logging.info('[apfaker] creating fake ap with ssid "%s" (bssid: %s) on channel %d', ssid, mac, channel)
-                agent.run(f"set wifi.ap.ssid {ssid}")
-                agent.run(f"set wifi.ap.bssid {mac}")
-                agent.run(f"set wifi.ap.channel {channel}")
-                agent.run("wifi.ap")
+                logging.info('[apfaker] creating fake ap with ssid "%s"', ssid)
+                frams.append(APFaker.create_beacon(ssid, password_protected=self.options['password_protected'])
                 agent.view().set('apfake', str(idx + 1))
             except Exception as ex:
                 logging.debug('[apfaker] %s', ex)
+
+        main_config = agent.config()
+
+        while not self.shutdown:
+            sendp(frames, iface=main_config['main']['iface'])
+            sleep(0.1)
+
+    def on_before_shutdown(self):
+        self.shutdown = True
 
     def on_ui_setup(self, ui):
         with ui._lock:
